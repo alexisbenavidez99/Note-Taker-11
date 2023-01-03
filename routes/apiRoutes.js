c7e9b7f8a14f609168c93f658b849b9dcaec7792
alexisbenavidez99 <callmealexis49@gmail.com>
2023-01-02 19:59:44 -0600

    adjusting post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
+const fs = require('fs');
 const path = require('path');
+const uuid = require('../helpers/uuid');
 
 // Creating API routes to read json file
-router.get('/api/tips', (req, res) => {
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+router.get('./db/db.json', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+router.post('../db/db.json', (req, res) => {
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+    const textString = JSON.stringify(newNote);
+    readAndAppend(newNote, '../db/db.json');
+    
+    fs.writeFile(`./db/${newNote.title}`, textString, (err) =>
+    err
+    ? console.error(err)
+    : console.log(`Review for ${newNote.title} has been written to JSON file`)) 
+    }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.use('/', htmlRoutes);
+app.use('/', apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
